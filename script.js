8b55097a0b81f400b4170de9be4603d5fbaaa55b
fitsociety-hue <fitsociety.hue@gmail.com>
2025-12-12 17:26:29 +0900

    Update Apps Script URL and switch to new repo

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
  */
 
 // --- CONFIGURATION ---
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz4XEFgjJTk6FwbgpeNPg1D7BRcS2-npDG-vedcpTAZDaZiyLO5ezBgJ8QphrJPM4xBmQ/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw9mu7yMN-QNv09L9XJBVlaeEP3pfjHMXurYvGCE3HBS7tifalSsi7IJ4u0IJlrdcyxyA/exec';
 
 // --- STATE MANAGEMENT ---
 const state = {
